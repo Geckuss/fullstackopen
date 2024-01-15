6a1c93a8c095dadda486ac57162cb091be613538
Geckuss <scrobalt@gmail.com>
2024-01-15 18:03:03 +0200

    1.8 done

--- 1/unicafe/src/App.jsx
+++ 1/unicafe/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistics = props => {
+  return(
+    <div>
+      <h2>Results</h2>
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All votes: {props.allVotes}</p>
+      <p>Average: {props.average}</p>
+      <p>Positive: {props.positive}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -15,19 +29,19 @@ const App = () => {
       const newGood = good + 1
       setGood(newGood);
       setAllVotes(newGood + neutral + bad);
-      setAverage(((newGood + bad*-1) / (newGood + neutral + bad))*100)
+      setAverage((newGood + bad*-1) / (newGood + neutral + bad))
       setPositive(newGood/(newGood+neutral+bad)*100)
     } else if (type === 'Neutral') {
       const newNeutral = neutral + 1
       setNeutral(newNeutral);
       setAllVotes(good + newNeutral + bad);
-      setAverage(((good + newNeutral*0 + bad*-1) / (good + newNeutral + bad))*100)
+      setAverage((good + newNeutral*0 + bad*-1) / (good + newNeutral + bad))
       setPositive(good/(good+newNeutral+bad)*100)
     } else if (type === 'Bad') {
       const newBad = bad + 1
       setBad(newBad);
       setAllVotes(good + neutral + newBad);
-      setAverage(((good + newBad*-1) / (good + neutral + newBad))*100)
+      setAverage((good + newBad*-1) / (good + neutral + newBad))
       setPositive(good/(good+neutral+newBad)*100)
     }
     
@@ -39,14 +53,9 @@ const App = () => {
       <button onClick={() => handleButtonClick('Good')}>Good</button>
       <button onClick={() => handleButtonClick('Neutral')}>Neutral</button>
       <button onClick={() => handleButtonClick('Bad')}>Bad</button>
-
-      <h2>Results</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All votes: {allVotes}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}%</p>
+      <div>
+        <Statistics good = {good} neutral = {neutral} bad = {bad} allVotes = {allVotes} average = {average} positive = {positive}/>
+      </div>
     </div>
   )
 }

508745db247bc02af791a51f086e09fa5afe0603
Geckuss <scrobalt@gmail.com>
2024-01-21 19:12:32 +0200

    2.2 done

--- 2/kurssitiedot/src/components/Course.jsx
+++ 2/kurssitiedot/src/components/Course.jsx
@@ -15,11 +15,14 @@ const Part = ({ part }) => {
 };
 
 const Content = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <p><strong>Total exercises: {totalExercises}</strong></p>
     </div>
   );
 };

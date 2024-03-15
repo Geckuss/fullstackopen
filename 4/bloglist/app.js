8f83cb26e42c2ab1bb755a0468651683636f9ffb
Geckuss <scrobalt@gmail.com>
2024-03-15 17:04:21 +0200

    restructured code to have app.js and index.js separate, changed imports to ES

---
+++ 4/bloglist/app.js
@@ -0,0 +1,41 @@
+import express, { json } from 'express'
+const app = express()
+import cors from 'cors'
+import { Schema, model, connect } from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const blogSchema = Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = model('Blog', blogSchema)
+
+const mongoUrl = process.env.DB_URL
+connect(mongoUrl)
+
+app.use(cors())
+app.use(json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+export default app
